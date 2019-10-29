67e49d4b8469aa80af164b5b60bd8f3bda02a406
Uchaiaye marma <43565322+uchaiaye53@users.noreply.github.com>
2019-10-30 02:46:31 +0600

    Add files via upload

---
+++ Euler-Method.m
@@ -0,0 +1,12 @@
+function retval = euler_method (f, xrange, y0, h)
+  pkg load symbolic;
+  syms x;
+  f=inline(f);
+  x = xrange(1):h:xrange(2); 
+  y(1) = y0
+  
+  for i = 2:length(x)
+    y(i) = y(i-1)+h*f(x(i-1), y(i-1));
+  endfor
+  retval = [x' y'];
+endfunction
